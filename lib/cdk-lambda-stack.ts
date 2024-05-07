d3a062b969b5ecce354e2a5fb891e6f6b9d4b625
lukejoliat <lukejoliat@gmail.com>
2024-05-07 16:37:58 -0400

    Add stack

--- lib/cdk-lambda-stack.ts
+++ lib/cdk-lambda-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "MyPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("lukejoliat/cdk-lambda", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
