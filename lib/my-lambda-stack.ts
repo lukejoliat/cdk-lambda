473c7a2699189c79959b0208fca8473fe9a696bb
lukejoliat <lukejoliat@gmail.com>
2024-05-08 10:56:45 -0400

    Update lambda code

--- lib/my-lambda-stack.ts
+++ lib/my-lambda-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Function, InlineCode, Runtime } from "aws-cdk-lib/aws-lambda";
-
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 export class MyLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new Function(this, "LambdaFunction", {
       runtime: Runtime.NODEJS_18_X,
-      handler: "index.handler",
-      code: new InlineCode('exports.handler = _ => "Hello, CDK - from Luke.";'),
+      handler: "handler.handler",
+      code: Code.fromAsset(path.join(__dirname, "lambda")),
     });
   }
 }
